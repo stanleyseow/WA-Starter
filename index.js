b0664ef7ef7cee841107e7db93bfbe7d11ad8b2a
stanleyseow <stanleyseow@gmail.com>
2025-04-16 11:47:15 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+}) // end of ready
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+}) // end of qr
+
+client.on('message_create', async message => {
+    if (message.body.startsWith('!ping')) {
+        console.log(`${message.from} sent a message: ${message.body}`);
+        // send back "pong" to the chat the message was sent in
+        await client.sendMessage(message.from, 'pong');
+    } 
+}) // end of message_create
+
+client.initialize();
